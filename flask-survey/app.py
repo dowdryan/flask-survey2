f36845ca29d210662269e21afa74a700bce6e46c
Ryan <dowdryan8@gmail.com>
2023-06-02 18:38:47 -0400

    Finished Part One

--- flask-survey/app.py
+++ flask-survey/app.py
@@ -1,12 +1,86 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import surveys, Survey, Question
+from surveys import surveys, Survey
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "verySecretSurveyKey"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
+# =============================================================================
+# Global Variables
+responses = []
+
+title = surveys["satisfaction"].title
+instructions = surveys["satisfaction"].instructions
+questions = surveys["satisfaction"].questions
+
+# =============================================================================
 @app.route("/")
-def app_page():
-    return render_template("base.html")
+def welcome_user():
+    return render_template("base.html",
+                           title=title,
+                           instructions=instructions)
+
+@app.route("/redirect", methods=["GET", "POST"])
+def begin_survey():
+    return redirect("/question")
+
+@app.route("/question", methods=["GET", "POST"])
+def get_question_num():
+    global num, responses
+    num = int(request.args.get("num", 0))
+
+    if num > len(questions):
+        flash("Invalid Number.")
+        print("Invalid Number.")
+        responses = []
+        return redirect("/question")
+
+    if num <= len(questions) - 1:
+        if request.form is None:
+            pass
+        elif request.method == "POST":
+            try:
+                response = request.form.get("userInput")
+                # Prevents user from adding 2 of the same answer when the go back a page.
+                if len(responses) >= num and responses[num -1] == response:
+                    pass
+                else:
+                    responses.append(response)
+            except:
+                pass
+        num += 1
+        print(responses)
+        print(f"num: {num}")
+        print(f"The length of questions is: {len(questions)}")
+        return render_template("questions.html",
+                            title=title,
+                            questions=questions,
+                            num=num,
+                            question=questions[num - 1].question,
+                            choices=questions[num - 1].choices)
+    elif num >= len(questions):
+        return redirect("/thank_you")
+
+@app.route("/thank_you")
+def thank_user():
+    global num
+    print(f"question_number: {num}")
+    print(f"The length of questions is: {len(questions)}")
+    return render_template("thanks.html")
+
+@app.route("/reset", methods=["GET", "POST"])
+def reset_survey():
+    global responses, num
+    responses = []
+    num = 0
+    return redirect("/")
+
+# Run the Flask app# =============================================================================
+if __name__ == "__main__":
+    app.run()
+
+
+# Get q.Parameter for num in the back end (Sent by front end).
+# Need to handle query Parameter.
--- flask-survey/surveys.py
+++ flask-survey/surveys.py
@@ -1,23 +1,20 @@
+# =============================================================================
 class Question:
     """Question on a questionnaire."""
 
     def __init__(self, question, choices=None, allow_text=False):
         """Create question (assume Yes/No for choices."""
-
         if not choices:
             choices = ["Yes", "No"]
-
         self.question = question
         self.choices = choices
         self.allow_text = allow_text
 
-
+# =============================================================================
 class Survey:
     """Questionnaire."""
-
     def __init__(self, title, instructions, questions):
         """Create questionnaire."""
-
         self.title = title
         self.instructions = instructions
         self.questions = questions
@@ -32,6 +29,7 @@ satisfaction_survey = Survey(
         Question("On average, how much do you spend a month on frisbees?",
                  ["Less than $10,000", "$10,000 or more"]),
         Question("Are you likely to shop here again?"),
+        Question("Do you see this message?"),
     ])
 
 personality_quiz = Survey(
